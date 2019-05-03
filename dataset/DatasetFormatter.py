1a2422f4210637bbfe5d39cca7ce3a76bc47a7f1
heorhii-berezovskyi <45767015+heorhii-berezovskyi@users.noreply.github.com>
2019-05-03 13:46:54 +0300

    Refactoring: increase readability of DatasetFormatter and ImageToLettersParser by adding parse of input arguments and splitting into simple methods.

--- dataset/DatasetFormatter.py
+++ dataset/DatasetFormatter.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import numpy as np
@@ -6,14 +7,14 @@ from numpy import ndarray
 
 class DatasetFormatter:
     """
-     Converts data set from csv to .npy format.
+     Converts emnist data set from csv to .npy format.
     """
 
     def __init__(self, num_of_channels: int, image_size: int):
         self.num_of_channels = num_of_channels
         self.image_size = image_size
 
-    def to_npy(self, dataset_path: str, new_dir: str, split_name: str, skip_header=0):
+    def to_npy(self, dataset_path: str, new_dir: str, split_name: str, skip_header: int):
         dataset = self._load(path_from=dataset_path, skip_header=skip_header)
         labels, data = self._split_into_labels_and_data(dataset=dataset)
 
@@ -30,7 +31,7 @@ class DatasetFormatter:
         return image
 
     @staticmethod
-    def _load(path_from: str, skip_header=0) -> ndarray:
+    def _load(path_from: str, skip_header: int) -> ndarray:
         return np.genfromtxt(path_from, delimiter=',', skip_header=skip_header, dtype=np.uint8)
 
     @staticmethod
@@ -38,3 +39,34 @@ class DatasetFormatter:
         labels = dataset[:, 0]
         data = dataset[:, 1:]
         return labels, data
+
+
+def run(args):
+    formatter = DatasetFormatter(num_of_channels=args.num_channels,
+                                 image_size=args.image_size)
+
+    formatter.to_npy(dataset_path=args.dataset,
+                     new_dir=args.write_to,
+                     split_name=args.set_name,
+                     skip_header=args.skip_header)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Trains specified model with specified parameters.')
+    parser.add_argument('--num_channels', type=int, help='Number of channels in images.', default=1)
+    parser.add_argument('--image_size', type=int, help='Size of the image.', default=28)
+
+    parser.add_argument('--dataset', type=str, help='Path to dataset file in csv format.',
+                        default=r'C:\Users\heorhii.berezovskyi\Documents\emnist\emnist-balanced-train.csv')
+
+    parser.add_argument('--write_to', type=str, help='Directory to save extracted labels and data.',
+                        default=r'C:\Users\heorhii.berezovskyi\Documents\emnist_balanced')
+
+    parser.add_argument('--set_name', type=str, help='Type of the dataset(train, test).',
+                        default='train')
+
+    parser.add_argument('--skip_header', type=int, help='Number of rows to skip in the dataset',
+                        default=0)
+
+    _args = parser.parse_args()
+    run(_args)
--- dataset/ImageToLettersParser.py
+++ dataset/ImageToLettersParser.py
@@ -1,52 +1,99 @@
+import argparse
 import os
 
 import cv2
+from numpy import ndarray
 
 
-def sort_contours(contours: list, method="left-to-right") -> (list, list):
-    # initialize the reverse flag and sort index
-    reverse = False
-    i = 0
+class ImageToLettersParser:
+    def __init__(self, min_letter_width: int, max_letter_width: int, min_letter_height: int, max_letter_height: int):
+        self.min_w = min_letter_width
+        self.max_w = max_letter_width
+        self.min_h = min_letter_height
+        self.max_h = max_letter_height
 
-    # handle if we need to sort in reverse
-    if method == "right-to-left" or method == "bottom-to-top":
-        reverse = True
+    def extract_letters(self, orig_image: ndarray, sorted_contours: list) -> list:
+        letters = []
+        for c in sorted_contours:
+            # Returns the location and width,height for every contour
+            x, y, w, h = cv2.boundingRect(c)
+            if self.min_w < w < self.max_w and self.min_h < h < self.max_h:
+                new_img = orig_image[y:y + h, x:x + w]
+                letters.append(new_img)
+        return letters
 
-    # handle if we are sorting against the y-coordinate rather than
-    # the x-coordinate of the bounding box
-    if method == "top-to-bottom" or method == "bottom-to-top":
-        i = 1
+    @staticmethod
+    def find_contours(image: ndarray) -> list:
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # construct the list of bounding boxes and sort them from top to
-    # bottom
-    bounding_boxes = [cv2.boundingRect(c) for c in contours]
-    contours, bounding_boxes = zip(*sorted(zip(contours, bounding_boxes),
-                                           key=lambda b: b[1][i], reverse=reverse))
+        thresh, img_bin = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    # return the list of sorted contours and bounding boxes
-    return contours, bounding_boxes
+        # Find contours for image, which will detect all the boxes
+        im2, contours, hierarchy = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
+
+    @staticmethod
+    def save_letters(letters: list, path_to: str):
+        for i in range(len(letters)):
+            cv2.imwrite(os.path.join(path_to, (str(i) + '.png')), letters[i])
+
+    @staticmethod
+    def sort_contours(contours: list, method="left-to-right") -> (list, list):
+        # initialize the reverse flag and sort index
+        reverse = False
+        i = 0
+
+        # handle if we need to sort in reverse
+        if method == "right-to-left" or method == "bottom-to-top":
+            reverse = True
+
+        # handle if we are sorting against the y-coordinate rather than
+        # the x-coordinate of the bounding box
+        if method == "top-to-bottom" or method == "bottom-to-top":
+            i = 1
+
+        # construct the list of bounding boxes and sort them from top to
+        # bottom
+        bounding_boxes = [cv2.boundingRect(c) for c in contours]
+        contours, bounding_boxes = zip(*sorted(zip(contours, bounding_boxes),
+                                               key=lambda b: b[1][i], reverse=reverse))
+
+        # return the list of sorted contours and bounding boxes
+        return contours, bounding_boxes
+
+
+def run(args):
+    parser = ImageToLettersParser(min_letter_width=args.min_letter_width,
+                                  max_letter_width=args.max_letter_width,
+                                  min_letter_height=args.min_letter_height,
+                                  max_letter_height=args.max_letter_height)
+
+    image_to_parse = cv2.imread(args.image)
+
+    contours = parser.find_contours(image=image_to_parse)
+
+    sorted_contours, _ = parser.sort_contours(contours=contours,
+                                              method="top-to-bottom")
+
+    letters = parser.extract_letters(orig_image=image_to_parse,
+                                     sorted_contours=sorted_contours)
+
+    parser.save_letters(letters=letters,
+                        path_to=args.write_to)
 
 
 if __name__ == "__main__":
-    # Read the image
-    img = cv2.imread(r'C:\Users\heorhii.berezovskyi\Documents\letters\7.tif')
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    parser = argparse.ArgumentParser(description='Trains specified model with specified parameters.')
+    parser.add_argument('--min_letter_width', type=int, help='Minimal width of a letter on image.', default=400)
+    parser.add_argument('--max_letter_width', type=int, help='Maximal width of a letter on image.', default=600)
 
-    thresh, img_bin = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    parser.add_argument('--min_letter_height', type=int, help='Minimal height of a letter on image.', default=400)
+    parser.add_argument('--max_letter_height', type=int, help='Maximal height of a letter on image.', default=600)
 
-    # Find contours for image, which will detect all the boxes
-    im2, contours, hierarchy = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Sort all the contours by top to bottom.
-    contours, boundingBoxes = sort_contours(contours, method="top-to-bottom")
+    parser.add_argument('--image', type=str, help='Image to parse path.',
+                        default=r'C:\Users\heorhii.berezovskyi\Documents\letters\1.tif')
+    parser.add_argument('--write_to', type=str, help='Directory to save letters.',
+                        default=r'C:\Users\heorhii.berezovskyi\Documents\LettersDataset\1')
 
-    cropped_dir_path = r'C:\Users\heorhii.berezovskyi\Documents\LettersDataset\7'
-
-    idx = 0
-    for c in contours:
-        # Returns the location and width,height for every contour
-        x, y, w, h = cv2.boundingRect(c)
-        if 100 < w < 600 and 100 < h < 600:
-            new_img = img[y:y + h, x:x + w]
-            cv2.imwrite(os.path.join(cropped_dir_path, (str(idx) + '.png')), new_img)
-            # cv2.imwrite(cropped_dir_path + str(idx) + '.png', new_img)
-            idx += 1
+    _args = parser.parse_args()
+    run(_args)
